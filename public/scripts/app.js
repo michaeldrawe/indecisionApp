cd29feefe4c399f13864fd8e1a48b576a79d0da1
Michael Drawe <michaeldrawe@gmail.com>
2019-03-07 23:45:20 -0700

    march 7 2019

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,22 +1,84 @@
 "use strict";
 
+//Used so we know this file is being used
 console.log("App.js is running!");
+
+// create app object title/subtitle
+//user title/subtitle in the template
+
+// only render the subtitle (and p tag) if subtitle exists
+//render new p tag -- if options.length > 0 "here are your options" "no options"
+
+var app = {
+  title: "Indecision App",
+  subtitle: "Making decisions a little easier",
+  options: ["One", "Two"]
+};
+function getSubtitle(subtitle) {
+  if (subtitle) {
+    return React.createElement(
+      "p",
+      null,
+      subtitle
+    );
+  }
+}
 
 //JSX - JavaScript XML
 var template = React.createElement(
+  "div",
+  { style: { textAlign: "center" } },
+  React.createElement(
+    "h1",
+    null,
+    app.title
+  ),
+  React.createElement(
+    "p",
+    null,
+    app.subtitle ? app.subtitle : "No Subtitle"
+  ),
+  React.createElement(
+    "p",
+    null,
+    app.options.length > 0 ? "Here are your options" : "no options"
+  )
+);
+
+var user = {
+  name: "Michael",
+  age: 39,
+  location: ""
+};
+
+function getLocation(location) {
+  if (location) {
+    return React.createElement(
+      "p",
+      null,
+      "Location: ",
+      location
+    );
+  }
+}
+
+var templateTwo = React.createElement(
   "div",
   null,
   React.createElement(
     "h1",
     null,
-    "Did this change?"
+    user.name ? user.name : "Anonymous"
   ),
-  React.createElement(
+  user.age && user.age >= 18 && React.createElement(
     "p",
     null,
-    "This is new info"
-  )
+    "Age: ",
+    user.age
+  ),
+  getLocation(user.location)
 );
+
 var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,12 +1,52 @@
+//Used so we know this file is being used
 console.log("App.js is running!");
+
+// create app object title/subtitle
+//user title/subtitle in the template
+
+// only render the subtitle (and p tag) if subtitle exists
+//render new p tag -- if options.length > 0 "here are your options" "no options"
+
+var app = {
+  title: "Indecision App",
+  subtitle: "Making decisions a little easier",
+  options: ["One", "Two"]
+};
+function getSubtitle(subtitle) {
+  if (subtitle) {
+    return <p>{subtitle}</p>;
+  }
+}
 
 //JSX - JavaScript XML
 var template = (
-  <div>
-    <h1>Did this change?</h1>
-    <p>This is new info</p>
+  <div style={{ textAlign: "center" }}>
+    <h1>{app.title}</h1>
+    <p>{app.subtitle ? app.subtitle : "No Subtitle"}</p>
+    <p>{app.options.length > 0 ? "Here are your options" : "no options"}</p>
   </div>
 );
+
+var user = {
+  name: "Michael",
+  age: 39,
+  location: ""
+};
+
+function getLocation(location) {
+  if (location) {
+    return <p>Location: {location}</p>;
+  }
+}
+
+var templateTwo = (
+  <div>
+    <h1>{user.name ? user.name : "Anonymous"}</h1>
+    {user.age && user.age >= 18 && <p>Age: {user.age}</p>}
+    {getLocation(user.location)}
+  </div>
+);
+
 var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
